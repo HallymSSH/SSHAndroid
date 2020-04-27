d1673df444b0599480bff08f3a2d244433b97939
changhyunsim <50908525+changhyunsim@users.noreply.github.com>
2020-04-27 19:27:10 +0900

    200427 destination list

---
+++ app/src/main/java/com/ssh/capstone/safetygohome/DestinationList.java
@@ -0,0 +1,73 @@
+package com.ssh.capstone.safetygohome;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.skt.Tmap.TMapData;
+import com.skt.Tmap.TMapPOIItem;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+public class DestinationList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_destlist);
+    }
+
+    public void findAllPoi() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("POI 통합 검색");
+
+        final EditText input = new EditText(this);
+        builder.setView(input);
+
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                final String strData = input.getText().toString();
+                TMapData tmapdata = new TMapData();
+
+                tmapdata.findAllPOI(strData, new TMapData.FindAllPOIListenerCallback() {
+                    @Override
+                    public void onFindAllPOI(ArrayList<TMapPOIItem> poiItem) {
+                        for (int i = 0; i < poiItem.size(); i++) {
+                            TMapPOIItem  item = poiItem.get(i);
+/*
+                            LogManager.printLog("POI Name: " + item.getPOIName().toString() + ", " +
+                                    "Address: " + item.getPOIAddress().replace("null", "")  + ", " +
+                                    "Point: " + item.getPOIPoint().toString());
+
+ */
+                        }
+                    }
+                });
+            }
+        });
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+
+    }
+
+}
