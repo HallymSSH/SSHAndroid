3e79fc606be881e8383c5ce7d84f865c7427a965
tlgh0623 <wwwk25@naver.com>
2020-04-28 01:37:38 +0900

    date fix

--- app/src/main/java/com/ssh/capstone/safetygohome/profile.java
+++ app/src/main/java/com/ssh/capstone/safetygohome/profile.java
@@ -1,14 +1,19 @@
 package com.ssh.capstone.safetygohome;
 
 import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
 
 
 public class profile extends AppCompatActivity {
@@ -16,7 +21,7 @@ public class profile extends AppCompatActivity {
     Button btn_profileback, btn_birthday;
     ImageView profile_view;
     TextView textView_Date;
-    private DatePickerDialog.OnDateSetListener callbackMethod;
+    private DatePickerDialog.OnDateSetListener listener;
 
 
     @Override
@@ -36,10 +41,10 @@ public class profile extends AppCompatActivity {
         profile_view = (ImageView) findViewById((R.id.profile_image));
         textView_Date = (TextView)findViewById(R.id.birthday);
 
-        callbackMethod = new DatePickerDialog.OnDateSetListener() {
+        listener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                textView_Date.setText(year+"년"+month+"월"+dayOfMonth+"일");
+                textView_Date.setText(year+"년 "+month+"월 "+dayOfMonth+"일 ");
 
             }
         };
@@ -58,11 +63,23 @@ public class profile extends AppCompatActivity {
 
             }
         });
+        btn_birthday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
 
-    }
+                DatePickerDialog dialog = new DatePickerDialog(
+                        profile.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,listener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
 
-    public void onClickHandler(View view) {
-        DatePickerDialog dialog = new DatePickerDialog(this, callbackMethod, 2020,4,28);
-        dialog.show();
+
     }
 }
