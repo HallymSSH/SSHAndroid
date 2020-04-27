04d185b043d0df25fabee253164f768351d3d627
changhyunsim <50908525+changhyunsim@users.noreply.github.com>
2020-04-27 23:45:58 +0900

    statusbar fix

--- app/src/main/java/com/ssh/capstone/safetygohome/contactbook_window.java
+++ app/src/main/java/com/ssh/capstone/safetygohome/contactbook_window.java
@@ -9,13 +9,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class contactbook_window extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // requestWindowFeature(Window.FEATURE_NO_TITLE);  //타이틀바 없애기
         setContentView(R.layout.layout_contactbook);
-
 
 
     }
