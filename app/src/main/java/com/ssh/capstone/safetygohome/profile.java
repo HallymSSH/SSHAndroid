556584f1481728f75b0b945eaeffe2ff16cd8e1b
tlgh0623 <wwwk25@naver.com>
2020-04-30 19:08:36 +0900

    profile fix

--- app/src/main/java/com/ssh/capstone/safetygohome/profile.java
+++ app/src/main/java/com/ssh/capstone/safetygohome/profile.java
@@ -1,6 +1,9 @@
 package com.ssh.capstone.safetygohome;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -12,15 +15,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
 
 import java.util.Calendar;
 
 
 public class profile extends AppCompatActivity {
 
-    Button btn_profileback, btn_birthday;
+    Button btn_profileback, btn_birthday, btn_sex;
     ImageView profile_view;
-    TextView textView_Date;
+    TextView textView_Date, textView_sex;
     private DatePickerDialog.OnDateSetListener listener;
 
 
@@ -38,8 +42,10 @@ public class profile extends AppCompatActivity {
     public void setting() {
         btn_profileback = (Button) findViewById(R.id.btn_profileback);
         btn_birthday = (Button) findViewById(R.id.btn_birthday);
+        btn_sex = (Button) findViewById(R.id.btn_sex);
         profile_view = (ImageView) findViewById((R.id.profile_image));
         textView_Date = (TextView)findViewById(R.id.birthday);
+        textView_sex = (TextView) findViewById(R.id.textView_sex);
 
         listener = new DatePickerDialog.OnDateSetListener() {
             @Override
@@ -60,6 +66,9 @@ public class profile extends AppCompatActivity {
         profile_view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(getApplicationContext(), v);
+
+                getMenuInflater().inflate(R.menu.popup, popup.getMenu());
 
             }
         });
@@ -80,6 +89,27 @@ public class profile extends AppCompatActivity {
             }
         });
 
+        btn_sex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(1);
+            }
+        });
 
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        final String [] items = {"남성","여성"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(profile.this);
+        builder.setTitle("성별을 선택하세요");
+        builder.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                textView_sex.setText(items[which]);
+                dialog.dismiss(); // 누르면 바로 닫히는 형태
+            }
+        });
+        return builder.create();
     }
 }
