50fe0054ccd522f79a882dc3f03fdf6b02d84162
HChanWoo <cksdn1st@gmail.com>
2020-06-02 14:37:15 +0900

    2020.06.2

--- app/src/main/java/com/ssh/capstone/safetygohome/listview_add.java
+++ app/src/main/java/com/ssh/capstone/safetygohome/listview_add.java
@@ -2,7 +2,9 @@ package com.ssh.capstone.safetygohome;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.Window;
@@ -12,7 +14,7 @@ import android.widget.FrameLayout;
 
 public class listview_add extends Activity {
 
-    FrameLayout btn_yes,btn_no;
+    FrameLayout btn_yes,btn_no, btn_tobook;
     EditText edit_name, edit_num;
     String name, num;
     Intent ToBook;
@@ -50,6 +52,7 @@ public class listview_add extends Activity {
     public void setting() {
         btn_yes = (FrameLayout)findViewById(R.id.btn_yes);
         btn_no = (FrameLayout)findViewById(R.id.btn_no);
+        btn_tobook = (FrameLayout)findViewById(R.id.btn_tobook);
         edit_name = (EditText)findViewById(R.id.edit_name);
         edit_num = (EditText)findViewById(R.id.edit_num);
         ToBook = new Intent(com.ssh.capstone.safetygohome.listview_add.this,
@@ -79,5 +82,29 @@ public class listview_add extends Activity {
                 overridePendingTransition(0, 0);
             }
         });
+
+        btn_tobook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setData(ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivityForResult(intent,0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            Cursor cursor = getContentResolver().query(data.getData(), new String[]{ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                    ContactsContract.CommonDataKinds.Phone.NUMBER},null,null,null);
+            cursor.moveToFirst();
+            String sName = cursor.getString(0);
+            String sNumber = cursor.getString(1);
+            edit_name.setText(sName);
+            edit_num.setText(sNumber);
+        }
     }
 }
